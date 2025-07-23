3ba2c748d0025439c36df0dd3df4d805b51da463
Anushree <anushreers2009@gmail.com>
2025-07-23 13:45:37 -0500

    new variable: text view

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            var textView = "Change 3"
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
